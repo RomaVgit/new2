cc287d6ec1f6239db8bf82aa51fdba3d3975166d
RomaVgit <romavuber@gmail.com>
2023-03-25 00:01:31 +0200

    MFc

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+let numRandom = function (ofTimes) {
+	let num = 0;
+	let rand;
+	while (num < ofTimes) {
+		rand = Math.random() * 10;
+		rand = Math.round(rand);
+		console.log(rand);
+		num++;
+	}
+}
+
+numRandom(3);
