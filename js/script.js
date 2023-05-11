b1bedc5002ac6dda0046a3587c7f8d70b2e6492a
RomaVgit <romavuber@gmail.com>
2023-05-11 17:29:19 +0300

    randNum

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
+rand = Math.random() * 10;
+rand = Math.round(rand);
+console.log(rand);
+
 const one = document.querySelectorAll('.after')[0];
-console.log(one);
 const twoo = one.innerHTML;
-console.log(twoo);
-one.innerHTML = 'Yes';
-
-
-let numRandom = function (ofTimes) {
-	let num = 0;
-	let rand;
-	while (num < ofTimes) {
-		rand = Math.random() * 10;
-		rand = Math.round(rand);
-		console.log(rand);
-		num++;
-	}
-}
-
-numRandom(5);
+one.innerHTML = `random number: ${rand}`;
+console.log(one);
