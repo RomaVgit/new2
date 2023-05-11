5400cb3c40595b75e3aa5c97734a6bb0b9ac4557
RomaVgit <romavuber@gmail.com>
2023-05-11 20:51:07 +0300

    color

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 rand = Math.random() * 10;
 rand = Math.round(rand);
-console.log(rand);
 
 const one = document.querySelectorAll('.after')[0];
 const twoo = one.innerHTML;
 one.innerHTML = `random number: ${rand}`;
-console.log(one);
+
