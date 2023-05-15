a9f0e2266034a0709f85f12b03ffcc309af79fdb
RomaVgit <romavuber@gmail.com>
2023-05-16 00:07:00 +0300

    logic

--- js/script.js
+++ js/script.js
@@ -1,7 +1,25 @@
-rand = Math.random() * 10;
+rand = Math.random() * 100;
 rand = Math.round(rand);
 
-const one = document.querySelectorAll('.after')[0];
+const one = document.querySelector('.after');
 const twoo = one.innerHTML;
 one.innerHTML = `random number: ${rand}`;
 
+
+document.querySelector('.button-1').onclick = myClick;
+
+function myClick() {
+	let num = document.querySelector('.i-1').value;
+	document.querySelector('.out').innerHTML = `You enter: ${num}`;
+	document.querySelector('.i-1').value = '';
+	document.querySelector('.i-1').placeholder = 'try again';
+
+	if (rand == num) {
+		document.querySelector('.out').innerHTML = `You WIN!!!`;
+	} else if (rand > num) {
+		document.querySelector('.out').innerHTML = `You enter: ${num} i\'ts less.`;
+	} else if (rand < num) {
+		document.querySelector('.out').innerHTML = `You enter: ${num} i\'ts more.`;
+	};
+
+}
